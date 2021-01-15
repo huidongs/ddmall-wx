bd04a27079cbb9972b9870979e564d476f913c3f
huidongs <2804234243@qq.com>
2021-01-15 16:59:43 +0800

    commit

--- pages/auth/reset/reset.js
+++ pages/auth/reset/reset.js
@@ -1,66 +1,172 @@
-// pages/auth/reset/reset.js
+var api = require('../../../config/api.js');
+var check = require('../../../utils/check.js');
+
+var app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    mobile: '',
+    code: '',
+    password: '',
+    confirmPassword: ''
+  },
+  onLoad: function(options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    // 页面渲染完成
+
+  },
+  onReady: function() {
+
+  },
+  onShow: function() {
+    // 页面显示
+
+  },
+  onHide: function() {
+    // 页面隐藏
+
+  },
+  onUnload: function() {
+    // 页面关闭
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  sendCode: function() {
+    let that = this;
+    wx.request({
+      url: api.AuthRegisterCaptcha,
+      data: {
+        mobile: that.data.mobile
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+        if (res.data.errno == 0) {
+          wx.showModal({
+            title: '发送成功',
+            content: '验证码已发送',
+            showCancel: false
+          });
+        } else {
+          wx.showModal({
+            title: '错误信息',
+            content: res.data.errmsg,
+            showCancel: false
+          });
+        }
+      }
+    });
   },
+  startReset: function() {
+    var that = this;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    if (this.data.mobile.length == 0 || this.data.code.length == 0) {
+      wx.showModal({
+        title: '错误信息',
+        content: '手机号和验证码不能为空',
+        showCancel: false
+      });
+      return false;
+    }
 
+    if (!check.isValidPhone(this.data.mobile)) {
+      wx.showModal({
+        title: '错误信息',
+        content: '手机号输入不正确',
+        showCancel: false
+      });
+      return false;
+    }
+
+    if (this.data.password.length < 3) {
+      wx.showModal({
+        title: '错误信息',
+        content: '用户名和密码不得少于3位',
+        showCancel: false
+      });
+      return false;
+    }
+
+    if (this.data.password != this.data.confirmPassword) {
+      wx.showModal({
+        title: '错误信息',
+        content: '确认密码不一致',
+        showCancel: false
+      });
+      return false;
+    }
+
+    wx.request({
+      url: api.AuthReset,
+      data: {
+        mobile: that.data.mobile,
+        code: that.data.code,
+        password: that.data.password
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+        if (res.data.errno == 0) {
+          wx.navigateBack();
+        } else {
+          wx.showModal({
+            title: '密码重置失败',
+            content: res.data.errmsg,
+            showCancel: false
+          });
+        }
+      }
+    });
   },
+  bindPasswordInput: function(e) {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    this.setData({
+      password: e.detail.value
+    });
   },
+  bindConfirmPasswordInput: function(e) {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+    this.setData({
+      confirmPassword: e.detail.value
+    });
   },
+  bindMobileInput: function(e) {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    this.setData({
+      mobile: e.detail.value
+    });
   },
+  bindCodeInput: function(e) {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+    this.setData({
+      code: e.detail.value
+    });
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  clearInput: function(e) {
+    switch (e.currentTarget.id) {
+      case 'clear-password':
+        this.setData({
+          password: ''
+        });
+        break;
+      case 'clear-confirm-password':
+        this.setData({
+          confirmPassword: ''
+        });
+        break;
+      case 'clear-mobile':
+        this.setData({
+          mobile: ''
+        });
+        break;
+      case 'clear-code':
+        this.setData({
+          code: ''
+        });
+        break;
+    }
   }
 })
